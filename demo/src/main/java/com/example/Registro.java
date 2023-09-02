64001c45ffb0344e9292aaa72abbd38e61916d08
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-09-02 08:44:48 +0000

    Add class Registro w/ eliminar and agregar(incompleted) methods

---
+++ demo/src/main/java/com/example/Registro.java
@@ -0,0 +1,49 @@
+package com.example;
+
+public class Registro {
+    private Usuario[] registro;
+    private int numRegistros;
+
+    //Constructor, inicializamos numRegistros a 0, puesto que aún no hay registros
+    public Registro(int n) {
+        registro = new Usuario[n];
+        numRegistros = 0;
+    }
+
+    public Boolean agregar(Usuario u) {
+        if (numRegistros < registro.length) {
+            //@TODO Agregar el usuario al registro y organizarlo por id
+            //Aca solo se esta agregando el usuario al final del arreglo, deberia organizarse
+
+            registro[numRegistros] = u;
+            numRegistros++;
+            return true;
+        } else {
+            System.out.println("No se pueden agregar más registros");
+            return false;
+        }
+    }
+
+    public Usuario eliminar(long id){
+        int i = 0;
+        //Aca se esta buscando la posicion del usuario a eliminar
+        while(i < numRegistros && registro[i].getId() != id){
+            i++;
+        }
+        if(i == numRegistros){
+            System.out.println("No se encontró el usuario");
+            return null;
+        }else{
+            Usuario eliminado = registro[i];
+            //Aca se esta eliminando el usuario y se esta reorganizando el arreglo
+            for(int j = i; j < numRegistros - 1; j++){
+                registro[j] = registro[j+1];
+            }
+            //Aca se esta actualizando el numero de registros
+            numRegistros--;
+            return eliminado;
+        }
+
+
+    }
+}
