988cc3b37b9f3b49d8acb0d8f655410df929e67a
Juan Manuel Teherán Machado <jteheranm@unal.edu.co>
2023-09-27 13:50:00 +0000

    Implement stack w/ SimpleList

--- demo/src/main/java/Stack/Stack.java
+++ demo/src/main/java/Stack/Stack.java
@@ -1,2 +1,37 @@
 package Stack;
 
+import SimpleList.List;
+
+public class Stack {
+    private List list;
+    private int size;
+
+    //Constructors
+    public Stack() {
+        this.list = new List();
+        this.size = list.getSize();
+    }
+
+    //Methods
+
+    public int getSize() {
+        return list.getSize();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public Object top() {
+        return list.First();
+    }
+
+    public void push(Object e) {
+        list.addFirst(e);
+    }
+
+    public Object pop() {
+        return list.removeFirst();
+    }
+
+}
